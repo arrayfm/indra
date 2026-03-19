a6e77bd5b4c548d75ff52d989a8221bab4e8672b
Alex Simpson <alxsimpson@gmail.com>
2026-03-19 14:50:39 +0000

    Add footer in

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,12 +1,18 @@
+import { cn } from '@/lib/utils/class-name'
+import { typePPMori } from '@/lib/utils/font'
+
 export const Footer = () => {
   return (
     <footer>
-      <div className="container">
-        <div className="grid grid-cols-12 py-4">
-          <div className="col-span-6"></div>
-          <nav className="col-span-6">
-            <ul className="flex items-end justify-end gap-5"></ul>
-          </nav>
+      <div className="container py-5">
+        <div className="text-grey-400 flex max-w-55 flex-col gap-3">
+          <p className={cn(typePPMori({ size: 'sm', weight: 'semibold' }))}>
+            Company information
+          </p>
+          <p className={cn(typePPMori({ size: 'xs' }))}>
+            © 2025 RPH Medica Limited.
+            <br /> All rights reserved. INDRA is a trademark of RPH Medica Ltd.
+          </p>
         </div>
       </div>
     </footer>

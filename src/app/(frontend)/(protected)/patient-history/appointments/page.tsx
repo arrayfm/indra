47a3411a018cabe26f5c2babdd3316b98d6d083c
Alex Simpson <alxsimpson@gmail.com>
2026-03-24 11:01:19 +0000

    Custom offset because seconds

--- src/app/(frontend)/(protected)/patient-history/appointments/page.tsx
+++ src/app/(frontend)/(protected)/patient-history/appointments/page.tsx
@@ -32,13 +32,19 @@ export function formatBookingDate(start: string): string {
 }
 
 export function formatBookingTime(start: string, end?: string): string {
+  const TIME_WITH_OFFSET: Intl.DateTimeFormatOptions = {
+    hour: 'numeric',
+    minute: '2-digit',
+    timeZoneName: 'short',
+  }
+
   const startDt = DateTime.fromISO(start)
   const endDt = end ? DateTime.fromISO(end) : undefined
 
   const startTime = startDt.toLocaleString(
-    endDt ? DateTime.TIME_SIMPLE : DateTime.TIME_WITH_SHORT_OFFSET
+    endDt ? DateTime.TIME_SIMPLE : TIME_WITH_OFFSET
   )
-  const endTime = `${endDt?.toLocaleString(DateTime.TIME_WITH_SHORT_OFFSET)}`
+  const endTime = endDt?.toLocaleString(TIME_WITH_OFFSET)
 
   return end ? `${startTime} — ${endTime}` : startTime
 }

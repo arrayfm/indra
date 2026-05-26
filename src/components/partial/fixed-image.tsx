8b602eada20ce01fd848240368e82c9473f6827b
joelarray <joel@joelbaker.net>
2026-05-26 12:35:30 +0100

    image tweak

--- src/components/partial/fixed-image.tsx
+++ src/components/partial/fixed-image.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 
 export const FixedImage = () => {
   return (
-    <div className="pointer-events-none fixed inset-0 -z-10 flex h-screen items-end justify-end border-2 opacity-10 sm:opacity-50">
+    <div className="pointer-events-none fixed top-0 right-0 left-0 -z-10 flex h-screen items-end justify-end overflow-hidden opacity-25 sm:right-[-25%] sm:left-[25%] sm:opacity-75">
       <Image
-        height={865}
-        width={549}
-        src="/images/bg.jpg"
-        alt="Meditating person"
+        height={800}
+        width={800}
+        src="/images/bg_blob.png"
+        alt="Meditation orb"
         loading="eager"
-        className="h-full w-full object-cover sm:w-1/2"
+        className="h-full w-full object-contain"
       />
     </div>
   )
